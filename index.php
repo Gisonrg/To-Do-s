34390e1151a58dfc5865ea39733072495783e8c8
lozy219 <uygnim.iel@gmail.com>
2014-02-16 17:23:53 +0800

    no message

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php 
 require_once("config/config.inc");
+session_save_path(SESSION_SAVED);
+session_start();
 require_once("model/db.php");
 require_once("controller/userController.php");
 require("view/header.inc");
 require("view/footer.inc");
-// session_save_path(SESSION_SAVED);
-session_start();
+
 
 $_SESSION['userID'] = 1;
 
