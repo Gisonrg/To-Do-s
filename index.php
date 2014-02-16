584a7a89a014b7776b495f2f545c6e16b1d1b64b
lozy219 <uygnim.iel@gmail.com>
2014-02-16 17:16:20 +0800

    no message

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php 
-
+require_once("config/config.inc");
+require_once("model/db.php");
+require_once("controller/userController.php");
 require("view/header.inc");
 require("view/footer.inc");
+// session_save_path(SESSION_SAVED);
+session_start();
+
+$_SESSION['userID'] = 1;
+
 showHeader("Home Page");
 
 
