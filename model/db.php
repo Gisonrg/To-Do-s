7133fbb40791eb6cedea953bafc090b20a57006a
lozy219 <uygnim.iel@gmail.com>
2014-02-19 00:44:00 +0800

    no message

--- model/db.php
+++ model/db.php
@@ -282,7 +282,7 @@ function retrieve_current_events() {
 		if ($a['id'] == $b['id']) {
 			return 0;
 		} else {
-			return $a['id'] > $b['id']? 1 : -1;
+			return $a['id'] > $b['id']? -1 : 1;
 		}
 	}
 
