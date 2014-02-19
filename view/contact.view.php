07c5ffafe04a51fcad3ced79cc3c8524d3e30c96
lozy219 <uygnim.iel@gmail.com>
2014-02-19 23:08:26 +0800

    about

---
+++ view/contact.view.php
@@ -0,0 +1,29 @@
+<!--contact.view.php-->
+<!DOCTYPE html>
+	<html lang="en">
+	<?php include('common/header.inc');?>
+	<body>
+		<?php include('common/top.inc');?>
+		
+		<div class="container container-pad">	
+
+		<div class="content-about">
+			<div class="view-about">
+				<p>A&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CP3101B</p>
+				<p>ASSIGNMENT</p>
+				<p>CREAT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BY</P>
+				<img src="static/img/JS.gif">
+				<img src="static/img/LMY.gif">
+				<h3>Jiang Sheng & Lei Mingyu</h3>
+ 			<br />
+		</div>			
+		</div>			
+
+		<!-- End of container -->	
+		</div>
+		<?php include('common/footer.inc');?>		
+	</body>
+</html>
+
+
+
